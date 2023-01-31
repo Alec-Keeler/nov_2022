a1c128060d24adda0cc322265907667d52efe90a
Alec Keeler <alec.keeler@gmail.com>
2023-01-30 19:00:39 -0600

    w10d1

---
+++ w10/d1/practice-for-week-10-express-route-handlers-main/server/app.js
@@ -0,0 +1,35 @@
+// Phase 2
+const {
+  getAllArtists,
+  getLatestArtist,
+  getArtistByArtistId,
+  addArtist,
+  editArtistByArtistId,
+  deleteArtistByArtistId,
+  getAlbumsForLatestArtist,
+  getAlbumsByArtistId,
+  getAlbumByAlbumId,
+  addAlbumByArtistId,
+  editAlbumByAlbumId,
+  deleteAlbumByAlbumId,
+  getFilteredAlbums,
+  getSongsByArtistId,
+  getSongsByAlbumId,
+  getSongBySongId,
+  addSongByAlbumId,
+  editSongBySongId,
+  deleteSongBySongId
+} = require('./data');
+
+const express = require('express');
+const app = express();
+
+// Your code here
+app.use(express.json())
+
+app.get(['/artists', '/musicians'], (req, res) => {
+  res.json(getAllArtists())
+})
+
+const port = 5000;
+app.listen(port, () => console.log('Server is listening on port', port));
